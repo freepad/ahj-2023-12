043fa133ef522c99819a299bde1c8e67e488cbb1
Damir Rysaev <freepad@ya.ru>
2023-12-15 19:53:54 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import { greet } from "./module";
+
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+export default function demo(value) {
+  return `Demo: ${value}`;
+}
+
+greet("Валентин");
+
+console.log("app.js included");
